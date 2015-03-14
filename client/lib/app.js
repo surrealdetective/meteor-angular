62db643aa2aed51750d79f8052481f1ef5ff902d
surrealdetective <thefifth@gmail.com>
2015-03-14 18:49:22 -0400

    move .ng.html template files to /views and ref in routes

--- client/lib/app.js
+++ client/lib/app.js
@@ -16,23 +16,3 @@ function($scope, $meteor){
   };
 
 }]);
-
-// controller
-angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteor',
-function($scope, $stateParams, $meteor){
-
-  $scope.party = $meteor.object(Parties, $stateParams.partyId, false);
-
-  $scope.save = function() {
-    $scope.party.save().then(function(numberOfDocs){
-      console.log('save success doc affected ', numberOfDocs);
-    }, function(error){
-      console.log('save error', error);
-    });
-  };
-
-  $scope.reset = function() {
-    $scope.party.reset();
-  };
-
-}]);
